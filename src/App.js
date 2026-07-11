0e4652b22d040419dcd9c826284d1460a02c1c65
Jordan Dickson <jfdickson25@gmail.com>
2026-07-11 10:39:33 -0600

    Add local storage so information persists between sessions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './App.css';
+import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRotateLeft } from "@fortawesome/free-solid-svg-icons";
 export default function App() {
 
     const [demis, setDemis] = React.useState([
@@ -20,11 +23,66 @@ export default function App() {
         powers: null
     });
 
+    useEffect(() => {
+        // Set up local storage for demis so the information persists across page reloads
+        const storedDemis = localStorage.getItem("demis");
+        const storedScores = localStorage.getItem("scores");
+        if(storedDemis) {
+            setDemis(JSON.parse(storedDemis));
+        }
+        if(storedScores) {
+            setScores(JSON.parse(storedScores));
+        }
+    }, []);
+
     return (
         <React.Fragment>
             <div id="title-area">
                 <img src={`${process.env.PUBLIC_URL}/images/title-and-logo.png`} alt="Scales of Fate" id="title-image"/>
             </div>
+            <FontAwesomeIcon icon={faArrowRotateLeft} id="refresh-icon" onClick={() => {
+                const icon = document.getElementById("refresh-icon");
+                icon.classList.add("refreshing");
+                
+                setTimeout(() => {
+                    icon.classList.remove("refreshing");
+
+                    setDemis([
+                        { name: "Pentha", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Agamar", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Klar", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Aponi", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Saghari", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Namari", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Naka", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Belan", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Isabel", status: { yes: false, no: false, maybe: false}, notes: "" }
+                    ]);
+                    setScores({
+                        position: null,
+                        guess: null,
+                        powers: null
+                    }); 
+
+                    localStorage.setItem("demis", JSON.stringify([
+                        { name: "Pentha", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Agamar", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Klar", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Aponi", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Saghari", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Namari", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Naka", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Belan", status: { yes: false, no: false, maybe: false}, notes: "" },
+                        { name: "Isabel", status: { yes: false, no: false, maybe: false}, notes: "" }
+                    ]));
+                    localStorage.setItem("scores", JSON.stringify({
+                        position: null,
+                        guess: null,
+                        powers: null
+                    }));
+                }, 500);
+
+            }} />
             <div id="demis-area">
                 {
                         demis.map((demi, index) => (
@@ -46,6 +104,7 @@ export default function App() {
                                                     demis[index].status.no = false;
                                                     demis[index].status.maybe = false;
                                                     setDemis([...demis]);
+                                                    localStorage.setItem("demis", JSON.stringify(demis));
                                             }}
                                         >✓</div>
                                         <div className="status status-no"
@@ -55,6 +114,7 @@ export default function App() {
                                                 demis[index].status.yes = false;
                                                 demis[index].status.maybe = false;
                                                 setDemis([...demis]);
+                                                localStorage.setItem("demis", JSON.stringify(demis));
                                             }}
                                         >X</div>
                                         <div className="status status-maybe"
@@ -64,6 +124,7 @@ export default function App() {
                                                 demis[index].status.yes = false;
                                                 demis[index].status.no = false;
                                                 setDemis([...demis]);
+                                                localStorage.setItem("demis", JSON.stringify(demis));
                                             }}
                                         >?</div>
                                     </div>
@@ -73,6 +134,7 @@ export default function App() {
                                     onChange={(event) => {
                                         demis[index].notes = event.target.value;
                                         setDemis([...demis]);
+                                        localStorage.setItem("demis", JSON.stringify(demis));
                                     }}
                                 />
                             </div>
@@ -101,6 +163,7 @@ export default function App() {
                                 ...scores,
                                 position: parseInt(event.target.value) || 0
                             });
+                            localStorage.setItem("scores", JSON.stringify(scores));
                         }} 
                     />
                     <input type="number" className="score-item" value={scores.guess} placeholder="0" 
@@ -117,6 +180,7 @@ export default function App() {
                                 ...scores,
                                 guess: parseInt(event.target.value) || 0
                             });
+                            localStorage.setItem("scores", JSON.stringify(scores));
                         }} 
                     />
                     <input type="number" className="score-item" value={scores.powers} placeholder="0"
@@ -134,6 +198,7 @@ export default function App() {
                                 ...scores,
                                 powers: value > 0 ? -value : value
                             });
+                            localStorage.setItem("scores", JSON.stringify(scores));
                         }} 
                     />
                     <div id="total" className="score-item">{ (scores.position || 0) + (scores.guess || 0) + (scores.powers || 0)}</div>
